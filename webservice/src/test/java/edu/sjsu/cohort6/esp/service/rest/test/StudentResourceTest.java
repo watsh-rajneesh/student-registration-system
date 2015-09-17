ca5c12ba8b3d577ad01f9551e2e3f46623b66080
rwatsh <watsh.rajneesh@sjsu.edu>
2015-09-17 09:32:14 -0700

    Adding dropwizard integration. To run it, execute mvn package and then "java -jar target/webservice-1.0-SNAPSHOT.jar server" and then go to browser and type: http://localhost:8080/students and it shoud return the test data for now as JSON.

---
+++ webservice/src/test/java/edu/sjsu/cohort6/esp/service/rest/test/StudentResourceTest.java
@@ -0,0 +1,12 @@
+package edu.sjsu.cohort6.esp.service.rest.test;
+
+/**
+ * Created by rwatsh on 9/17/15.
+ */
+public class StudentResourceTest {
+
+    @org.testng.annotations.Test
+    public void testGetStudent() throws Exception {
+
+    }
+}
