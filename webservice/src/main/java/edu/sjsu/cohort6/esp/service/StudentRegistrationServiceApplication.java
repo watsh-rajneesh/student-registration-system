9fa7e80e5d3c41441315c1104925693ef5da5857
rwatsh <watsh.rajneesh@sjsu.edu>
2015-09-19 10:01:26 -0700

    Removed DB client initialization code as it is moved to managed DBConfig class.

--- webservice/src/main/java/edu/sjsu/cohort6/esp/service/StudentRegistrationServiceApplication.java
+++ webservice/src/main/java/edu/sjsu/cohort6/esp/service/StudentRegistrationServiceApplication.java
@@ -14,12 +14,9 @@
 
 package edu.sjsu.cohort6.esp.service;
 
-import com.google.inject.Guice;
 import com.google.inject.Inject;
-import com.google.inject.Module;
 import edu.sjsu.cohort6.esp.dao.DBClient;
 import edu.sjsu.cohort6.esp.dao.DBFactory;
-import edu.sjsu.cohort6.esp.dao.DatabaseModule;
 import edu.sjsu.cohort6.esp.service.health.DBHealthCheck;
 import edu.sjsu.cohort6.esp.service.rest.StudentResource;
 import io.dropwizard.Application;
@@ -48,9 +45,7 @@ public class StudentRegistrationServiceApplication extends Application<StudentRe
     @Override
     public void initialize(final Bootstrap<StudentRegistrationServiceConfiguration> bootstrap) {
         // TODO: application initialization
-        Module module = new DatabaseModule();
-        Guice.createInjector(module).injectMembers(this);
-        client = dbFactory.create("localhost", 27017, "esp");
+
     }
     @Override
     public void run(StudentRegistrationServiceConfiguration studentRegistrationServiceConfiguration, Environment environment) throws Exception {
