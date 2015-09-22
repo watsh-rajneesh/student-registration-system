b59e153804f1cd1ca43bf46a8a7650b12abc745e
rwatsh <watsh.rajneesh@sjsu.edu>
2015-09-22 00:40:54 -0700

    Fixed it per further learning of angularjs.

--- webservice/src/main/resources/assets/js/app.js
+++ webservice/src/main/resources/assets/js/app.js
@@ -2,12 +2,12 @@
 (function() {
   var app = angular.module('studentModule', []);
 
-  app.controller('StudentController', function($scope, $http){
+  app.controller('StudentController', ['$http', function($http){
+    var store = this;
+    store.students = [];
+
     $http.get("/api/students").success(function(data) {
-        var jsonString = angular.toJson(data);
-        console.log(jsonString)
-        $scope.students = angular.fromJson(jsonString)
-        console.log(students)
+        store.students = data;
     });
-  });
+  }]);
 })();
